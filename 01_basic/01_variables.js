e66af8780503882052a2e4fee987909b3f044615
Supriya Shukla <shuklasupriya251@gmail.com>
2026-08-17 15:49:34 +0530

    basic in javascript

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const userID=189292;
+console.log(userID);
+let city="Delhi";
+var accountPassword="1108"
+ accountPassword="2938"
+ city="bangluru"
+ console.table([city,userID,accountPassword])
+/*
+prefer not to use var because of issue in block  scpoe  and functional scope
+*/
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as newer version
+//alert(3+3) // we are using nodejs ,not browser
+let name="supriya"
+let age=18
+let city=null;
+console.log(typeof null)
+/*
+number=2 to power 53
+bigint
+string=""
+boolean=true/false
+null=standalone value ,it is an object
+undefined
+symbol=unique
+object
+*/
