735094d824222d8a41a5da99011cd2ea68c725c4
Supriya Shukla <shuklasupriya251@gmail.com>
2026-08-18 11:47:40 +0530

    conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score= null
+console.log(score)
+console.log( typeof score)
+
+let valueInNumber = Number(score)
+console.log( typeof valueInNumber)
+console.log(valueInNumber)
+//"99" => 99
+//null=> 0
+//undefined=> null
+//"abc"=> NaN(Not A Number)
+//true=>1;false=>0  
+let isLogged= " ";
+let booleanisLogged= Boolean(isLogged)
+
+console.log(booleanisLogged)
+//1=>true;0=>false
+//"t"=>true;" "=>true
