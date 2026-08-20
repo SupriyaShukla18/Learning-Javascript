c48e854b53aa3168b15ac430df4e9a231dbdeaac
Supriya Shukla <shuklasupriya251@gmail.com>
2026-08-20 14:11:22 +0530

    string datatype done

---
+++ 01_basic/04_String.js
@@ -0,0 +1,24 @@
+const name="supriya shukla"
+const repo="50"
+
+console.log(`hey my name is ${name} and i have ${repo} repo `)
+// `` this is called template literals or template string(backtick)
+//it is used so that multiple lines of string  u can write
+//it is help so that write the variable into  string interpolation(${})
+ // we can also write the string into object form
+
+ const gamename=new String("hellocination")
+ //new keyword is used to make the object in heap
+ console.log(typeof gamename)
+ console.log(gamename.__proto__);
+ console.log(gamename.length)
+ console.log(gamename.toUpperCase())
+ console.log(gamename)
+ console.log(gamename.charAt(3))
+ console.log(gamename.indexOf("c"))
+ console.log(gamename.substring(2,6))
+console.log(gamename.slice(2,-8))
+console.log(gamename.replace("l","k"))
+console.log(gamename.includes("hel"))
+console.log(gamename)
+console.log(gamename.split())
